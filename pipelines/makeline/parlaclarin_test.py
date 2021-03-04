8b1f8f98396df0333404b3b7931f151c61ead50f
Roger Mähler <roger.mahler@hotmail.com>
2021-03-04 08:42:55 +0100

    Added initial xslt make pipeline

---
+++ pipelines/makeline/parlaclarin_test.py
@@ -0,0 +1,14 @@
+
+# def test_parla_clarin_to_csv():
+
+#     # test_filename = './tests/test_data/prot-1956-höst-fk--26.xml'
+#     test_filename = './tests/test_data/prot-199394--124.xml'
+
+#     parser = ParlaClarinXml2Csv()
+
+#     csv_str = parser.read_transform(test_filename)
+
+#     assert csv_str is not None
+
+#     with open('./tests/output/parla_clarin.csv', 'w') as fp:
+#         fp.write(csv_str)
