6912608bf9433f97741100e2c1e329f8daa2b689
Roger Mähler <roger.mahler@hotmail.com>
2021-03-08 10:39:46 +0100

    Added path helper

---
+++ workflow/rules/__paths__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def find_root(d: str) -> str:
+    return os.path.join(os.getcwd().split(d)[0], d)
+
+
+def fix_path():
+    sys.path.insert(0, find_root("parla_clarin_pipeline"))
