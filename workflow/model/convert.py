7b402a53465993789d5324c903e3373f4c002a45
Roger Mähler <roger.mahler@hotmail.com>
2021-03-08 10:35:52 +0100

    Added dedent filter

--- workflow/model/convert.py
+++ workflow/model/convert.py
@@ -1,8 +1,14 @@
+import textwrap
 from typing import Union
 
-from jinja2 import Environment,  PackageLoader, Template, select_autoescape
+from jinja2 import Environment, PackageLoader, Template, select_autoescape, Undefined
 
 from . import entities as model
+
+def dedent(value: str) -> str:
+    if isinstance(value, Undefined):
+        raise ValueError("dedent: jinja2.Undefined value string encountered")
+    return textwrap.dedent(value) if value is not None else ""
 
 
 jinja_env = Environment(
@@ -11,7 +17,7 @@ jinja_env = Environment(
     trim_blocks=True,
     lstrip_blocks=True,
 )
-
+jinja_env.filters['dedent'] = dedent
 
 class ProtocolConverter:
     def __init__(self, template: Union[str, Template]):
