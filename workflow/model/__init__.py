f5dd979464dc9ce004333d7570c9a7697491c577
Roger Mähler <roger.mahler@hotmail.com>
2021-03-12 11:10:46 +0100

    Added missing exports

--- workflow/model/__init__.py
+++ workflow/model/__init__.py
@@ -1,3 +1,5 @@
 # type: ignore
-from .convert import ProtocolConverter
-from .entities import Protocol, Speech, SpeechFactory
+
+from .compute import WordFrequencyCounter, compute_word_frequencies
+from .convert import ProtocolConverter, convert_protocol
+from .entities import ParlaClarinSpeechTexts, Protocol, Speech, SpeechFactory
