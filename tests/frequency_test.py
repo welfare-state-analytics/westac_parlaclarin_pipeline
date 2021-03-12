fd0f409c9571bc36ed5525f8a54f9cd25ccdb72d
Roger Mähler <roger.mahler@hotmail.com>
2021-03-12 11:07:18 +0100

    Added word frequency counter

---
+++ tests/frequency_test.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import pytest
+from workflow.model import WordFrequencyCounter
+from workflow.model.entities import ParlaClarinSpeechTexts
+
+sys.path.append((lambda d: os.path.join(os.getcwd().split(d)[0], d))("westac_parlaclarin_pipeline"))
+
+
+def test_parla_clarin_swallow():
+    filenames = ['tests/test_data/prot-199293--72.xml']
+    texts = ParlaClarinSpeechTexts(filenames)
+    i = 0
+    for text in texts:
+        assert len(text) > 0
+        i += 1
+    assert i > 0
+
+
+@pytest.mark.parametrize('text', ["a a b c c d e f a e", ["a a b c c", "d e f a e"]])
+def test_word_frequency_counter(text):
+
+    counter: WordFrequencyCounter = WordFrequencyCounter()
+
+    counter.swallow(text)
+
+    assert counter.frequencies.get('a', None) == 3
+    assert counter.frequencies.get('b', None) == 1
+    assert counter.frequencies.get('c', None) == 2
+    assert counter.frequencies.get('d', None) == 1
+    assert counter.frequencies.get('e', None) == 2
+    assert counter.frequencies.get('f', None) == 1
+
+
+def test_word_frequency_counter_swallow_parla_clarin_files():
+    filenames = ['tests/test_data/prot-199293--72.xml']
+    texts = ParlaClarinSpeechTexts(filenames)
+    counter: WordFrequencyCounter = WordFrequencyCounter('/data/swedish-bert-models/bert-base-swedish-cased')
+
+    counter.swallow(texts)
+
+    assert len(counter.frequencies) > 0
+
+
+def test_persist_word_frequencies():
+
+    filenames = ['tests/test_data/prot-199293--72.xml']
+    texts = ParlaClarinSpeechTexts(filenames)
+    counter: WordFrequencyCounter = WordFrequencyCounter()
+
+    counter.swallow(texts)
+
+    store_name = 'tests/output/test_persist_word_frequencies.pkl'
+    counter.store(store_name)
+
+    assert os.path.isfile(store_name)
+
+    wf = WordFrequencyCounter.load(store_name)
+    assert counter.frequencies == wf
+
+    os.unlink(store_name)
---
+++ workflow/model/compute.py
@@ -0,0 +1,74 @@
+import os
+import pickle
+from collections import defaultdict
+from glob import glob
+from typing import Callable, Iterable, List, Union
+
+from tqdm.auto import tqdm
+
+from .entities import ParlaClarinSpeechTexts
+from .tokenize import tokenize as default_tokenize
+from .utility import logger
+
+
+class WordFrequencyCounter:
+    def __init__(self, tokenize: Callable[[str], List[str]] = None, do_lower_case=True):
+        self.tokenize = tokenize or default_tokenize
+        self.frequencies = defaultdict(int)
+        self.do_lower_case = do_lower_case
+
+    def swallow(self, value: Union[str, Iterable[str], ParlaClarinSpeechTexts]) -> "WordFrequencyCounter":
+        texts = (
+            (value,)
+            if isinstance(value, str)
+            else (t for _, t in value)
+            if isinstance(value, ParlaClarinSpeechTexts)
+            else value
+        )
+        for text in tqdm(texts):
+            if self.do_lower_case:
+                text = text.lower()
+            for word in self.tokenize(text):
+                self.frequencies[word] += 1
+        return self
+
+    def store(self, filename: str, cut_off: int = None) -> None:
+        if cut_off:
+            frequencies = {w: c for w, c in self.frequencies if c > cut_off}
+        else:
+            frequencies = self.frequencies
+        with open(filename, 'wb') as fp:
+            pickle.dump(frequencies, fp, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def load(filename: str) -> defaultdict(int):
+        with open(filename, 'rb') as fp:
+            return pickle.load(fp)
+
+
+def compute_word_frequencies(source: Union[str, List[str]], filename: str) -> None:
+    try:
+        if isinstance(source, ParlaClarinSpeechTexts):
+            texts = source
+        else:
+            if isinstance(source, str):
+                if os.path.isfile(source):
+                    filenames = [source]
+                elif os.path.isdir(source):
+                    filenames = glob.glob(os.path.join(source, "*.xml"))
+                else:
+                    filenames = glob.glob(source)
+            elif isinstance(source, list):
+                filenames = source
+            else:
+                raise ValueError(f"unknown source of type {type(source)}")
+
+            texts = ParlaClarinSpeechTexts(filenames)
+
+        counter = WordFrequencyCounter()
+
+        counter.swallow(texts).store(filename)
+
+    except Exception as ex:
+        logger.exception(ex)
+        raise
