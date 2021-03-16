2bbf948816bf55a94a34c6f54e2f0a442f10b020
Roger Mähler <roger.mahler@hotmail.com>
2021-03-16 20:55:08 +0100

    Added guard

--- workflow/model/tokenize.py
+++ workflow/model/tokenize.py
@@ -7,18 +7,20 @@ from sparv.modules.segment.segment import BetterWordTokenizer
 MODEL_NAME = 'models/segment/bettertokenizer.sv'
 SALDO_TOKENS_NAME = 'models/segment/bettertokenizer.sv.saldo-tokens'
 
-tokenizer = None
+_tokenizer = None
 
 
 def tokenize(text: str) -> List[str]:
-    global tokenizer
-    tokenizer = tokenizer or BetterWordTokenizer(
+    global _tokenizer
+    if paths.data_dir is None:
+        raise ValueError("run sparv setup to specify SPARV_DATADIR")
+    _tokenizer = _tokenizer or BetterWordTokenizer(
         model=os.path.join(paths.data_dir, MODEL_NAME),
         token_list=os.path.join(
             paths.data_dir,
             SALDO_TOKENS_NAME,
         ),
     )
-    span_tokens = list(tokenizer.span_tokenize(text))
+    span_tokens = list(_tokenizer.span_tokenize(text))
     tokens = [text[x:y] for x, y in span_tokens]
     return tokens
