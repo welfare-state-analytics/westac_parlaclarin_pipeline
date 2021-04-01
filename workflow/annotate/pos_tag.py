a5ec7e869e51dcaaadcda7e8dca9be3901ed6f2e
Roger Mähler <roger.mahler@hotmail.com>
2021-04-02 00:42:29 +0200

    Stanza bulk process

--- workflow/annotate/pos_tag.py
+++ workflow/annotate/pos_tag.py
@@ -1,4 +1,3 @@
-
 import os
 import pathlib
 import zipfile
@@ -7,26 +6,32 @@ from typing import List
 import pandas as pd
 from stanza.models.common.doc import Document
 
-from ..model.convert import dedent, dehyphen
 from ..model.entities import Protocol
 from ..model.utility import strip_extensions
 from .stanza_tagger import StanzaTagger
 
 
-def annotate_speeches(tagger: StanzaTagger, protocol: Protocol) -> List[dict]:
+def document_to_csv(tagged_document: Document, sep='\t') -> str:
+    """Converts a stanza.Document to a TSV string"""
+    csv_str = '\n'.join(f"{w.text}{sep}{w.lemma}{sep}{w.upos}{sep}{w.xpos}" for w in tagged_document.iter_words())
+    csv_str = f"text{sep}lemma{sep}pos{sep}xpos\n{csv_str}"
+    return csv_str
+
+
+def tag_speeches(tagger: StanzaTagger, protocol: Protocol) -> List[dict]:
 
     speech_items = []
     speech_index = 1
+    speech_texts = []
 
     for speech in protocol.speeches:
 
-        text: str = dehyphen(dedent(speech.text)).strip()
+        text: str = speech.text.strip()
 
         if not text:
             continue
 
-        speech_document: Document = tagger.to_document(text)
-        speech_csv = tagger.document_to_csv(speech_document)
+        speech_texts.append(text)
         document_name = f"{strip_extensions(protocol.name)}@{speech_index}"
         speech_items.append(
             {
@@ -34,14 +39,20 @@ def annotate_speeches(tagger: StanzaTagger, protocol: Protocol) -> List[dict]:
                 'speaker': speech.speaker or "Unknown",
                 'speech_date': protocol.date,  # speech.speech_date or
                 'speech_index': speech_index,
-                'annotation': speech_csv,
-                'document_name': f"{1}",
+                'annotation': "",
+                'document_name': document_name,
                 'filename': f"{document_name}.csv",
-                'num_tokens': speech_document.num_tokens,
-                'num_words': speech_document.num_words,
+                'num_tokens': 0,
+                'num_words': 0,
             }
         )
         speech_index += 1
+
+    documents: List[Document] = tagger.tag(speech_texts)
+    for i, document in enumerate(documents):
+        speech_items[i]['annotation'] = document_to_csv(document)
+        speech_items[i]['num_tokens'] = document.num_tokens
+        speech_items[i]['num_words'] = document.num_words
 
     return speech_items
 
@@ -54,11 +65,8 @@ def write_to_zip(output_filename: str, speech_items: dict) -> None:
             fp.writestr(item['filename'], item['annotation'])
             del item['annotation']
 
-        document_index_csv: str = pd.DataFrame(speech_items).to_csv(sep='\t', header=True)
-        fp.writestr(
-            'document_index.csv',
-            document_index_csv,
-        )
+        document_index_str: str = pd.DataFrame(speech_items).to_csv(sep='\t', header=True)
+        fp.writestr('document_index.csv', document_index_str)
 
 
 def annotate_protocol(
@@ -77,6 +85,6 @@ def annotate_protocol(
         pathlib.Path(output_filename).touch(exist_ok=True)
         return
 
-    speech_items = annotate_speeches(tagger, protocol)
+    tagged_speeches = tag_speeches(tagger, protocol)
 
-    write_to_zip(output_filename, speech_items)
+    write_to_zip(output_filename, tagged_speeches)
