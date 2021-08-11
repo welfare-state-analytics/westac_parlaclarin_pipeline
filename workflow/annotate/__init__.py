c2934cecebca0a2a2d14777f787eb765e89d0440
Roger Mähler <roger.mahler@hotmail.com>
2021-08-11 09:18:44 +0200

    Fixed export of renamed file

--- workflow/annotate/__init__.py
+++ workflow/annotate/__init__.py
@@ -1,4 +1,4 @@
 # type: ignore
 
 from .annotate import annotate_protocol, document_to_csv, tag_speeches, write_to_zip
-from .stanza_tagger import StanzaDocument, StanzaTagger
+from .stanza import StanzaDocument, StanzaTagger
