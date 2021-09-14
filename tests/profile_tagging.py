85bf8e75a5d76ec3f42742084f9b868928a30f02
Roger Mähler <roger.mahler@hotmail.com>
2021-09-14 12:43:23 +0200

    make tidy

--- tests/profile_tagging.py
+++ tests/profile_tagging.py
@@ -5,41 +5,48 @@ from shutil import rmtree
 from typing import List
 
 import pyriksprot
+import pytest
 import snakemake
 from workflow.config.typed_config import Config, load_typed_config
 from workflow.taggers import StanzaTagger, TaggerRegistry
 
+from .utility import setup_working_folder
+
 # from utility import setup_working_folder  # pylint: disable=import-error
 
 
-# def run_snakemake():
+@pytest.mark.skip(reason="Very slow")
+@pytest.mark.slow
+def run_snakemake():
 
-#     test_protocols: List[str] = [
-#         'prot-1936--ak--8.xml',
-#         'prot-1961--ak--5.xml',
-#         'prot-1961--fk--6.xml',
-#         'prot-198687--11.xml',
-#         'prot-200405--7.xml',
-#         'prot-197778--160.xml'
-#     ]
+    test_protocols: List[str] = [
+        'prot-1936--ak--8.xml',
+        'prot-1961--ak--5.xml',
+        'prot-1961--fk--6.xml',
+        'prot-198687--11.xml',
+        'prot-200405--7.xml',
+        'prot-197778--160.xml',
+    ]
 
-#     workdir = aj("./tests/test_data/work_folder")
+    workdir = aj("./tests/test_data/work_folder")
 
-#     rmtree(workdir, ignore_errors=True)
-#     setup_working_folder(root_path=workdir, test_protocols=test_protocols)
+    rmtree(workdir, ignore_errors=True)
+    setup_working_folder(root_path=workdir, test_protocols=test_protocols)
 
-#     snakemake.snakemake(
-#         jj('workflow', 'Snakefile'),
-#         config=dict(
-#             config_filename=aj("./tests/test_data/test_config.yml"),
-#         ),
-#         debug=True,
-#         keep_target_files=True,
-#         cores=1,
-#         verbose=True,
-#     )
+    snakemake.snakemake(
+        jj('workflow', 'Snakefile'),
+        config=dict(
+            config_filename=aj("./tests/test_data/test_config.yml"),
+        ),
+        debug=True,
+        keep_target_files=True,
+        cores=1,
+        verbose=True,
+    )
 
 
+@pytest.mark.skip(reason="Very slow")
+@pytest.mark.slow
 def run_tag_protocol_xml():
 
     config_filename: str = aj("./tests/test_data/test_config.yml")
