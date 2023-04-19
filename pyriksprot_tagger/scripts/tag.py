1982c8b67ea5e4596bfba40405783a2d9a0d19d8
roger-mahler <roger.mahler@hotmail.com>
2023-04-19 11:32:45 +0200

    Improved script

--- pyriksprot_tagger/scripts/tag.py
+++ pyriksprot_tagger/scripts/tag.py
@@ -1,31 +1,51 @@
 import click
 from loguru import logger
 from pyriksprot.workflows.tag import ITagger, tag_protocols
-from pyriksprot_tagger import check_cuda
-from pyriksprot_tagger.taggers import StanzaTaggerFactory
+from pyriksprot_tagger.config import Config
+from pyriksprot_tagger.utility import check_cuda
 
 
 @click.command()
+@click.argument('config_filename')
 @click.argument('source_folder')
 @click.argument('target_folder')
-@click.argument('config_filename')
 @click.option('--force', is_flag=True, default=False, help='Force if exists')
 @click.option('--recursive', is_flag=True, default=True, help='Recurse subfolders')
-@click.option('--disable-gpu', is_flag=True, default=False, help='Disable GPU')
 def main(
-    source_folder: str = None,
-    target_folder: str = None,
+    config_filename: str,
+    source_folder: str,
+    target_folder: str,
     force: bool = False,
-    recursive: bool = False,
-    disable_gpu: bool = False,
+    recursive: bool = True,
 ) -> None:
+    tagit(
+        config_filename=config_filename,
+        source_folder=source_folder,
+        target_folder=target_folder,
+        force=force,
+        recursive=recursive,
+    )
 
+
+def tagit(
+    config_filename: str,
+    source_folder: str,
+    target_folder: str,
+    force: bool = False,
+    recursive: bool = True,
+):
     check_cuda()
 
-    tagger: ITagger = StanzaTaggerFactory(use_gpu=not disable_gpu).create()
+    config: Config = Config.load(config_filename)
+
+    tagger: ITagger = config.tagger_factory.create()
 
     tag_protocols(
-        tagger=tagger, source_folder=source_folder, target_folder=target_folder, force=force, recursive=recursive
+        tagger=tagger,
+        source_folder=source_folder,
+        target_folder=target_folder,
+        force=force,
+        recursive=recursive,
     )
 
     logger.info("workflow ended")
@@ -33,3 +53,4 @@ def main(
 
 if __name__ == "__main__":
     main()
+    # tagit("sample-data/config.yml", "sample-data/v0.6.0/parlaclarin/protocols/", "sample-data/v0.6.0/tagged_frames")
