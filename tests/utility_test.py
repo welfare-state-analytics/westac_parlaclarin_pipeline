2e61b13a38c21a5c031d45e34e21c852d0b5be53
roger-mahler <roger.mahler@hotmail.com>
2023-05-16 09:58:41 +0200

    Added missing text

--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,21 +1,24 @@
-from pyriksprot_tagger.utility import expand_basenames
+from pyriksprot_tagger.utility import expand_basenames, expand_target_files
 
+
+TEST_BASENAMES = [ 'prot-198687--11', 'prot-200405--7', 'prot-1961--fk--6', 'prot-1961--ak--5', 'prot-1936--ak--8', ]
 
 def test_expand_basenames():
     years, filenames = expand_basenames('tests/output/work_folder/riksdagen-corpus/corpus/protocols', 'xml')
     assert years == ['198687', '200405', '1961', '1961', '1936']
-    assert filenames == [
-        'prot-198687--11',
-        'prot-200405--7',
-        'prot-1961--fk--6',
-        'prot-1961--ak--5',
-        'prot-1936--ak--8',
-    ]
+    assert filenames == TEST_BASENAMES
 
 
-# def test_expand_target_files():
-#     assert expand_target_files(
-#         'tests/output/work_folder/riksdagen-corpus/corpus/protocols',
-#         'xml',
-#         ['198687', '200405', '1961', '1961', '1936'],
-#     ) == ['tests/output/work_folder/riksdagen-corpus/corpus/protocols/198687/prot-198687--11.xml']
+def test_expand_target_files():
+
+    source_folder = 'tests/output/work_folder/riksdagen-corpus/corpus/protocols'
+    source_extension = 'xml'
+
+    target_folder: str = 'tests/output'
+    target_extension = 'zip'
+
+    target_files: list[str] = expand_target_files(source_folder=source_folder, source_extension=source_extension, target_folder=target_folder, target_extension=target_extension, years=None)
+
+    assert target_files is not None
+
+    assert target_files == [f'tests/output/{x.split("-")[1]}/{x}.zip' for x in TEST_BASENAMES]
