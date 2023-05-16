f044610d51eeaa2d1e17e2d4ceb31a40f9f1f534
roger-mahler <roger.mahler@hotmail.com>
2023-05-16 10:00:21 +0200

    📌 make tidy

--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,7 +1,13 @@
 from pyriksprot_tagger.utility import expand_basenames, expand_target_files
 
+TEST_BASENAMES = [
+    'prot-198687--11',
+    'prot-200405--7',
+    'prot-1961--fk--6',
+    'prot-1961--ak--5',
+    'prot-1936--ak--8',
+]
 
-TEST_BASENAMES = [ 'prot-198687--11', 'prot-200405--7', 'prot-1961--fk--6', 'prot-1961--ak--5', 'prot-1936--ak--8', ]
 
 def test_expand_basenames():
     years, filenames = expand_basenames('tests/output/work_folder/riksdagen-corpus/corpus/protocols', 'xml')
@@ -17,7 +23,13 @@ def test_expand_target_files():
     target_folder: str = 'tests/output'
     target_extension = 'zip'
 
-    target_files: list[str] = expand_target_files(source_folder=source_folder, source_extension=source_extension, target_folder=target_folder, target_extension=target_extension, years=None)
+    target_files: list[str] = expand_target_files(
+        source_folder=source_folder,
+        source_extension=source_extension,
+        target_folder=target_folder,
+        target_extension=target_extension,
+        years=None,
+    )
 
     assert target_files is not None
 
