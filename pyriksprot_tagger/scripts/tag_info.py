e0275e4c79f48dd7ccd98e33418c6818c8a2c299
roger-mahler <roger.mahler@hotmail.com>
2024-09-23 21:20:22 +0200

    change: enhance tag_info command with configuration file and additional options

--- pyriksprot_tagger/scripts/tag_info.py
+++ pyriksprot_tagger/scripts/tag_info.py
@@ -2,7 +2,9 @@
 Prints tag & commit info (SHA & tag) for a Git repository.
 
 """
+
 import click
+from pyriksprot.configuration import ConfigStore, ConfigValue
 from pyriksprot.gitchen import GitInfo
 from pyriksprot.utility import write_yaml
 
@@ -10,16 +12,34 @@ from pyriksprot.utility import write_yaml
 
 
 @click.command()
-@click.argument('folder', type=click.STRING)
-@click.option('--tag', type=click.STRING, help='Print info for specified tag (default HEAD).', default=None)
+@click.argument('config_file', type=str)
+@click.argument('folder', type=str)
+@click.option('--tag', type=str, help='Print info for specified tag (default HEAD).', default=None)
+@click.option('--organisation', type=str, help='Github organisation.', default=None)
+@click.option('--repository', type=str, help='Github repository.', default=None)
 @click.option(
     '--key',
     type=click.Choice(["tag", "ref", "sha", "sha8", "tag_url", "commit_url"], case_sensitive=False),
     help='Prints value for given key',
     default=None,
 )
-def main(folder: str = None, tag: str = None, key: str = None):
-    data: dict = GitInfo(folder).tag_info(source='workdir', tag=tag)
+def main(
+    config_file: str,
+    folder: str = None,
+    tag: str = None,
+    organisation: str = None,
+    repository: str = None,
+    key: str = None,
+):
+    ConfigStore.configure_context(source=config_file)
+
+    tag = tag or ConfigValue("corpus.version").value
+    organisation = organisation or ConfigValue("corpus.github.user").value
+    repository = repository or ConfigValue("corpus.github.repository").value
+
+    data: dict = GitInfo(organisation=organisation, repository=repository, source_folder=folder).tag_info(
+        source='workdir', tag=tag
+    )
     if key:
         print(data.get(key, ""))
     else:
@@ -27,4 +47,4 @@ def main(folder: str = None, tag: str = None, key: str = None):
 
 
 if __name__ == "__main__":
-    main()
+    main()  # type: ignore # pylint: disable=no-value-for-parameter
