c081c06dae6a6536f464b07f6df0ddc4973402dd
roger-mahler <roger.mahler@hotmail.com>
2024-09-23 21:20:58 +0200

    change: update logging filename in snake-it.sh to include version

--- pyriksprot_tagger/scripts/other/snake-it.sh
+++ pyriksprot_tagger/scripts/other/snake-it.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source .env
+VERSION=v1.y.z
 export OMP_NUM_THREADS=16
 for ((year=1920;year<=2020;year++)); do
 	echo $year
-	nohup make annotate YEAR=$year PROCESSES_COUNT=1 &>> tag.${RIKSPROT_REPOSITORY_TAG}.log
+	nohup make annotate YEAR=$year PROCESSES_COUNT=1 &>> tag.${VERSION}.log
 done
